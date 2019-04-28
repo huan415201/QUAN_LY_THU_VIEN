784a25c267fca25f035134a9553e9c1cf4a1520e
Thuan <greydinh1105>
2019-04-28 15:55:55 +0700

    Thuan - thiet ke muon sach

--- src/frmTimTraMuonSach.java
+++ src/frmTimTraMuonSach.java
@@ -1,10 +1,29 @@
 
+import entity.Employee;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Vector;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 import org.fluttercode.datafactory.impl.DataFactory;
+import tools.ButtonEditor;
+import tools.ButtonRenderer;
+import tools.DisplayableObjectTableModel;
+import tools.ObjectTableModel;
+import tools.RamdomUtil;
+import org.jdesktop.swingx.calendar.SingleDaySelectionModel;
+import org.jdesktop.swingx.JXDatePicker;
+
+import javax.swing.*;
+import javax.swing.text.DefaultFormatterFactory;
+import javax.swing.text.DateFormatter;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.*;
+import java.awt.*;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -16,14 +35,24 @@ import org.fluttercode.datafactory.impl.DataFactory;
  * @author hau
  */
 public class frmTimTraMuonSach extends javax.swing.JFrame {
-
+    public static String bundlea = "abc";
     /**
      * Creates new form frmTimTraMuonSach
-     * 
+     *
      */
     TableModel tableModel = createTableModel();
+
+    ObjectTableModel<Employee> tableModelE = new DisplayableObjectTableModel<>(Employee.class);
+
+    DefaultTableModel dm = new DefaultTableModel();
+
     public frmTimTraMuonSach() {
+        dm.setDataVector(new Object[][]{{1,null,null,null,null,"Thêm"}}, new Object[]{"STT", "Masach", "TenSach", "TheLoai", "TacGia", "Button"});
+        tableModelE.setObjectRows(getEmployees());
         initComponents();
+        tbMuon.getColumn("Button").setCellRenderer(new ButtonRenderer());
+        tbMuon.getColumn("Button").setCellEditor(
+                new ButtonEditor(new JCheckBox(),dm));
         setExtendedState(JFrame.MAXIMIZED_BOTH);
     }
 
@@ -44,6 +73,19 @@ public class frmTimTraMuonSach extends javax.swing.JFrame {
         tbSach = new javax.swing.JTable(tableModel);
         jTextField2 = RowFilterUtil.createRowFilter(tbSach);
         jPanel2 = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        jComboBox1 = new javax.swing.JComboBox<>();
+        jLabel3 = new javax.swing.JLabel();
+        Date date = new Date();
+        jTextField1 = new DateTimePicker();
+        jTextField1.setFormats( DateFormat.getDateTimeInstance( DateFormat.SHORT, DateFormat.MEDIUM ) );
+        jTextField1.setTimeFormat( DateFormat.getTimeInstance( DateFormat.MEDIUM ) );
+
+        jTextField1.setDate(date);
+
+
+        jScrollPane3 = new javax.swing.JScrollPane();
+        tbMuon = new javax.swing.JTable();
 
         javax.swing.GroupLayout jFrame1Layout = new javax.swing.GroupLayout(jFrame1.getContentPane());
         jFrame1.getContentPane().setLayout(jFrame1Layout);
@@ -91,29 +133,65 @@ public class frmTimTraMuonSach extends javax.swing.JFrame {
                     .addComponent(jLabel1)
                     .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(26, 26, 26)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 177, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 203, Short.MAX_VALUE)
                 .addGap(79, 79, 79))
         );
 
-        jTabbedPane1.addTab("tab1", jPanel1);
+        jTabbedPane1.addTab("Tra cứu sách", jPanel1);
+
+        jLabel2.setText("Họ tên đọc giả");
+
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+
+        jLabel3.setText("Ngày mượn");
+
+        tbMuon.setModel(dm);
+        jScrollPane3.setViewportView(tbMuon);
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 506, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel2Layout.createSequentialGroup()
+                        .addGap(26, 26, 26)
+                        .addComponent(jLabel2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 198, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jLabel3)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jScrollPane3)))
+                .addGap(24, 24, 24))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 356, Short.MAX_VALUE)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(22, 22, 22)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel3)
+                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(67, 67, 67)
+                .addComponent(jScrollPane3, javax.swing.GroupLayout.DEFAULT_SIZE, 27, Short.MAX_VALUE)
+                .addGap(246, 246, 246))
         );
 
-        jTabbedPane1.addTab("tab2", jPanel2);
+        jTabbedPane1.addTab("Phiếu mượn sách", jPanel2);
 
         getContentPane().add(jTabbedPane1, java.awt.BorderLayout.CENTER);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -151,19 +229,39 @@ public class frmTimTraMuonSach extends javax.swing.JFrame {
     }
 
     private static TableModel createTableModel() {
-      TableModel md = new UserTableModel.SachTableModel();
-      return md;
+        TableModel md = new UserTableModel.SachTableModel();
+        return md;
+    }
+
+    public static List<Employee> getEmployees() {
+        final List<Employee> list = new ArrayList<>();
+        for (int i = 1; i <= 20; i++) {
+            Employee e = new Employee();
+            e.setName(RamdomUtil.getFullName());
+            e.setPhone(Integer.toString(RamdomUtil.getInt(111111111, 999999999)));
+            e.setDept(RamdomUtil.getAnyOf(Employee.DEPT_LIST));
+            e.setFullTime(RamdomUtil.getBoolean());
+            e.setDateOfBirth(RamdomUtil.getDate(1950, 2000));
+            list.add(e);
+        }
+        return list;
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JFrame jFrame1;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane3;
     private javax.swing.JSlider jSlider1;
     private javax.swing.JTabbedPane jTabbedPane1;
+    private DateTimePicker jTextField1;
     private javax.swing.JTextField jTextField2;
+    private javax.swing.JTable tbMuon;
     private javax.swing.JTable tbSach;
     // End of variables declaration//GEN-END:variables
 }
---
+++ src/tools/ButtonRenderer.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tools;
+
+import javax.swing.DefaultCellEditor;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.UIManager;
+import javax.swing.table.TableCellRenderer;
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.UIManager;
+
+/**
+ *
+ * @author hau
+ */
+public class ButtonRenderer extends JButton implements TableCellRenderer {
+
+  public ButtonRenderer() {
+    setOpaque(true);
+  }
+
+  public Component getTableCellRendererComponent(JTable table, Object value,
+      boolean isSelected, boolean hasFocus, int row, int column) {
+    if (isSelected) {
+      setForeground(table.getSelectionForeground());
+      setBackground(table.getSelectionBackground());
+    } else {
+      setForeground(table.getForeground());
+      setBackground(UIManager.getColor("Button.background"));
+    }
+    setText((value == null) ? "" : value.toString());
+    return this;
+  }
+}
+
+/**
+ * @version 1.0 11/09/98
+ */
+
